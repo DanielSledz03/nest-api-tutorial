5b2f5272f216215716c3d501c7abe21a49015404
DanielSledz03 <daniel.sledz@pluscode.io>
2023-09-22 22:29:00 +0200

    add jwt"

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+
 @Injectable({})
 export class AuthService {
   constructor(
